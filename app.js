07a8bc97628616d49dc02f6c2dab1e9049e3a6c0
I-MOHIT <mohitchugh180919@gmail.com>
2021-11-10 02:05:11 +0530

    Cookies

--- app.js
+++ app.js
@@ -32,39 +32,55 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('12345-67890-09876-54321'));
 
 function auth(req,res,next) {
-  console.log(req.headers);
+  console.log(req.signedCookies);
 
-  var authHeader = req.headers.authorization;
+  if(!req.signedCookies.user){
+    var authHeader = req.headers.authorization;
 
-  if(authHeader==null){
-    var err = new Error('You are not authenticated!');
+    if(authHeader==null){
+      var err = new Error('You are not authenticated!');
 
-    res.setHeader('WWW-Authenticate','Basic');
-    err.status = 401;
-    return next(err);
-  }
+      res.setHeader('WWW-Authenticate','Basic');
+      err.status = 401;
+      return next(err);
+    }
 
-  // Splits the base64 encoded string into two strings separated by a space,
-  // whose first element is the word Base and second is the encoded string,
-  // second element is taken and decoded as per base64 and then it is further
-  // split on the basis of a : containing the username:password
-  var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
+    // Splits the base64 encoded string into two strings separated by a space,
+    // whose first element is the word Base and second is the encoded string,
+    // second element is taken and decoded as per base64 and then it is further
+    // split on the basis of a : containing the username:password
+    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
 
-  var username = auth[0];
-  var password = auth[1];
+    var username = auth[0];
+    var password = auth[1];
 
-  if(username === 'admin' && password === 'password'){
-    next();
+    if(username === 'admin' && password === 'password'){
+      res.cookie('user','admin',{signed:true})
+      next();
+    }
+    else{
+      var err = new Error('Username and/or password is incorrect');
+      
+      res.setHeader('WWW-Authenticate','Basic');
+      err.status = 401;
+      return next(err);
+    }
   }
   else{
-    var err = new Error('Username and/or password is incorrect');
-    
-    res.setHeader('WWW-Authenticate','Basic');
-    err.status = 401;
-    return next(err);
+    if(req.signedCookies.user === 'admin'){
+      next();
+    }
+    else{
+      var err = new Error('You are not authenticated');
+      
+      //Must have been prompted earlier and this cookie is invalid
+      //res.setHeader('WWW-Authenticate','Basic');
+      err.status = 401;
+      return next(err);
+    }
   }
 }
 
